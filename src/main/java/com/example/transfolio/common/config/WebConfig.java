8b94c316f97a2a8d87442705891540d056be68bd
Hyeonchul <wkdgus1136@naver.com>
2024-04-07 13:03:32 +0900

    [f/m] 패키징 구조 변경

--- src/main/java/com/example/transfolio/common/config/WebConfig.java
+++ src/main/java/com/example/transfolio/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.transfolio.config;
+package com.example.transfolio.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
