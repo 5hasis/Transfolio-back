42b4a4af29b977fd3114f56c5f3047fb0c8e750b
Hyeonchul <wkdgus1136@naver.com>
2024-07-01 13:08:41 +0900

    [f/d] CORS 전역 허용 옵션 추가

--- src/main/java/com/example/transfolio/common/config/WebConfig.java
+++ src/main/java/com/example/transfolio/common/config/WebConfig.java
@@ -17,6 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(CORS_URL);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
