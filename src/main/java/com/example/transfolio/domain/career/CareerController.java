adb708a93c251f73b05f215685fc9fc0903fdbc6
ohasis <dhgkdud4002@naver.com>
2024-10-26 16:46:44 +0900

    경력 컨트롤러 추가

---
+++ src/main/java/com/example/transfolio/domain/career/CareerController.java
@@ -0,0 +1,9 @@
+package com.example.transfolio.domain.career;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/career")
+public class CareerController {
+}
